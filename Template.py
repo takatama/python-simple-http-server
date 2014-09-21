6ee0f1d8ad140a944d9ea505c443fef722db9116
takatama <h-takatama@biglobe.co.jp>
2014-09-22 00:45:26 +0900

    .tpl can use % for define python native command. % end must be used after defining the command.

--- Template.py
+++ Template.py
@@ -2,20 +2,29 @@ class Template:
     @staticmethod
     def render(file_name, model = {}):
         import re
-        source = ["buf = []\n"]
+        source = ['buf = []\n']
         with open(file_name) as f:
             data = f.read()
+        indent = ''
         for line in data.split('\n'):  
+            command_index = line.find('%')
+            if command_index >= 0:
+                if line.find('end') >= 0:
+                    indent = indent[4:]
+                else:
+                    source.append(indent + line[command_index + 1:].lstrip() + '\n')
+                    indent += '    '
+                continue
             while True:
                 m = re.search(r'\{\{(\w+)\}\}', line)
                 if not bool(m):
                     break
-                source.append("buf.append('%s')\n" % line[:m.start()])
-                source.append("buf.append(%s)\n" % m.group(1))
+                source.append(indent + 'buf.append("%s")\n' % line[:m.start()])
+                source.append(indent + 'buf.append(%s)\n' % m.group(1))
                 line = line[m.end():]
             if len(line) > 0:
-                source.append("buf.append('%s\\n')\n" % line)
-        source.append("buf = ''.join(buf)")
+                source.append(indent + 'buf.append("%s\\n")\n' % line)
+        source.append('buf = "".join(buf)')
         code = compile(''.join(source), '<string>', 'exec')
         exec code in model
         return model['buf']
