da2c01479df4630bc8906fc9cc2e0dfcc9c1328d
takatama <h-takatama@biglobe.co.jp>
2014-09-21 09:06:34 +0900

    GET /hello returns Hello World

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
 from BaseHTTPServer import HTTPServer
 from urlparse import urlparse
+from Hello import Hello
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        if (self.path == '/hello'):
+            hello = Hello()
+            response = hello.say()
+        else:
+            self.send_response(404) 
+            self.end_headers()
+            self.wfile.write("404 Not Found")
+            return
         self.send_response(200) 
         self.end_headers()
-        self.wfile.write("Hello World")
+        self.wfile.write(response)
         return
 
 address = ("", 8888)
