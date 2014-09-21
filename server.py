cc3945309208f82d467ffeadee92508b03401aef
takatama <h-takatama@biglobe.co.jp>
2014-09-21 15:18:07 +0900

    @get can user parameterized URL

--- server.py
+++ server.py
@@ -1,55 +1,55 @@
 import re
+import urlparse
 from BaseHTTPServer import BaseHTTPRequestHandler
 from BaseHTTPServer import HTTPServer
-import urlparse
 from Hello import Hello
 
 gets = {}
 
 def get(url):
     def _(f):
-        gets[url] = f
+        url_pattern = ''
+        params = []
+        url_list = url.split('/')
+        url_list.pop(0)
+        for u in url_list:
+            if u.find(':') == 0:
+               params.append(u.replace(':', ''))
+               u = '(\w+)'
+            url_pattern += '/' + u
+        gets[url_pattern] = f
     return _
 
 class RequestHandler(BaseHTTPRequestHandler):
 
     @get('/hello')
-    def say(self, query):
+    def say(self, query, *args):
         hello = Hello()
         name = query.get('name');
         if name is None:
             return hello.say()
         return hello.say(', '.join(name))
 
+    @get('/greeting/:name')
+    def greeting(self, query, *args):
+        hello = Hello()
+        return hello.say(args[0])
+
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
         path = parsed_path.path
         query = urlparse.parse_qs(parsed_path.query)
         for url, func in gets.iteritems():
-            print url
             m = re.match(url, path)
-            print bool(m)
             if bool(m):
-                message = func(self, query)
+                message = func(self, query, *m.groups())
                 self.send_response(200) 
                 self.end_headers()
                 self.wfile.write(message)
                 return
-
-        m = re.match(r'/greeting/(\w+)', path)
-        if bool(m):
-            hello = Hello()
-            name = m.group(1)
-            message = hello.say(name)
-        else:
-            self.send_response(404) 
-            self.end_headers()
-            self.wfile.write('404 Not Found')
-            return
-        self.send_response(200) 
+        self.send_response(404) 
         self.end_headers()
-        self.wfile.write(message)
-        return
+        self.wfile.write('404 Not Found')
 
 address = ('', 8888)
 server = HTTPServer(address, RequestHandler)
