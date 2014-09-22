db65fc4fed9d4eb1f9b49ea9c8e6c704cba33ff1
takatama <takatama.app+github@gmail.com>
2014-09-22 16:17:21 +0900

    @post is used for binding POST URL and the function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from server import get, run
+from server import get, post, run
 from Hello import Hello
+from Template import Template
 
 @get('/hello')
 def say(self, query, *args):
@@ -9,9 +10,18 @@ def say(self, query, *args):
         return hello.say()
     return hello.say(', '.join(name))
 
+@get('/hello/:name')
+def hello(self, query, *args):
+    return Template.render('hello.tpl', {"name": args[0]})
+
 @get('/greeting/:name')
 def greeting(self, query, *args):
     hello = Hello()
     return hello.say(args[0])
+
+@post('/comments')
+def comment(self, data, *args):
+    print data
+    return ''
 
 run(host = '', port = 8888)
